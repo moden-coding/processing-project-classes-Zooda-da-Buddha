5aefd2fa7ab12b5c1383c050fae91f6eef4e90f3
Zooda-da-Buddha <28jlevin@gannacademy.org>
2024-12-04 15:06:34 -0500

    Here

--- src/App.java
+++ src/App.java
@@ -1,20 +1,70 @@
 import processing.core.*;
 
 public class App extends PApplet{
+    double screen = 0.01;
+
+    final int STARTaCTION = 1;
+    final int ENDaCTION = 2;
+    final int uP = 3;
+    final int lEFT = 4;
+    final int dOWN = 5;
+    final int rIGHT = 6;
+
+    final double mp1 = 60;
+
+
+    Player thePlayer;
     public static void main(String[] args)  {
         PApplet.main("App");
     }
 
     public void setup(){
-        
+
+        thePlayer = new Player(0, 0, 5, 5, 50, 50, 0, 0.00, 5, false, this);
     }
 
     public void settings(){
-        
+        size(1460, 836);
+//      middle is x = 730 and y = 418
     }
 
     public void draw(){
-       
+        if (screen == 0.01) {
+            
+        }
 
+    }
+
+    public void keyPressed() {
+        if (screen % 1 == 0) {
+            if (key == 'w' || key == UP) {
+            }else if(key == 'a' || key == LEFT) {
+                thePlayer.moveManager(lEFT);
+            }else if(key == 's' || key == DOWN) {
+            }else if(key == 'd' || key == RIGHT) {
+            }
+        }
+
+    }
+
+
+    public boolean whereMouseClick(int rightOfMouseX, int leftOfMouseX, int underMouseY, int aboveMouseY) {
+        if (mouseX < rightOfMouseX && mouseX > leftOfMouseX && mouseY < underMouseY && mouseY > aboveMouseY) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void startScreen() {
+        background(70, 110, 30);
+        
+        if (mousePressed && whereMouseClick(630, 830, 557, 667)) {
+            double screen = mp1
+        }
+    }
+
+    public void MP1() {
+        background(165, 42, 42);
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,76 @@
+import processing.core.PApplet;
+
+public class Player {
+    final int STARTaCTION = 1;
+    final int ENDaCTION = 2;
+    final int uP = 3;
+    final int lEFT = 4;
+    final int dOWN = 5;
+    final int rIGHT = 6;
+
+    private int mana;
+    private int manaMax;
+
+    private int health;
+    private int healthMax;
+
+    private int Xpos;
+    private int Ypos;
+
+    private int speed;
+
+    private int inventory;
+
+    private double money;
+    
+    private boolean combat;
+
+    private PApplet canvas;
+
+    public Player(int mana, int manaMax, int health, int healthMax, int x, int y, int inventory, double money, int speed, boolean combat, PApplet c) {
+        this.mana = mana;
+        this.manaMax = manaMax;
+
+        this.health = health;
+        this.healthMax = healthMax;
+
+        this.Xpos = x;
+        this.Ypos = y;
+
+        this.inventory = inventory;
+
+        this.money = money;
+
+        this.speed = speed;
+
+        this.combat = combat;
+        canvas = c;
+    }
+
+    public void inCombat() {
+        combat = true;
+    }
+
+    public void damage(int damageDealt) {
+        health -= damageDealt;
+        if (health <= 0) {
+        }
+    }
+
+    public void refresh() {
+        canvas.triangle(Xpos, Ypos, Xpos - 10, Ypos + 20, Xpos + 10, Ypos + 20);
+    }
+
+    public void moveManager(int whichWay) {
+        if (whichWay == lEFT) {
+            this.Xpos -= this.speed;
+        }else if (whichWay == rIGHT) {
+            this.Xpos += this.speed;
+        }else if (whichWay == uP) {
+            this.Ypos -= this.speed;
+        }else if (whichWay == dOWN) {
+            this.Ypos += this.speed;
+        }
+    }
+    
+}
